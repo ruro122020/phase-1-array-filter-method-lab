c139d50ba9e7dd8da2fe8df9438d74bf5eb39dd1
RuthRojas <ruro122020@gmail.com>
2023-09-04 11:10:17 -0600

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution here
+
+function findMatching(arrayOfNames, stringName){
+  const newArray = arrayOfNames.filter((name)=>{
+    if(name.toLowerCase() === stringName.toLowerCase()){
+      return name
+    }
+  })
+  return newArray
+}
+function fuzzyMatch(arrayOfNames, stringName){
+  const newArray = arrayOfNames.filter((name)=>{
+    let lowerCaseName = name.toLowerCase()
+    if(lowerCaseName.slice(0,2) === stringName.toLowerCase()){
+      return name
+    }
+  })
+  return newArray
+}
+
+function matchName(arrayOfDrivers, driverName){
+  const newArray = arrayOfDrivers.filter((driverObj)=>{
+    if(driverObj.name.toLowerCase() === driverName.toLowerCase()){
+      return driverObj
+    }
+  })
+  return newArray
+}
